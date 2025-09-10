aa64674375faf4df55896867bc8e02e458842fc2
appaddbiz <appaddbiz@gmail.com>
2025-09-10 16:59:13 +0530

    (feat/home page):update homepage

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -42,7 +42,7 @@ export const metadata = {
 const page = () => {
   return (
     <Layout pageTitle="home">
-      {/* <TopBar /> */}
+      <TopBar />
       <Header />
       <HeaderCloned />
       {/* <MainSliderOne /> */}
