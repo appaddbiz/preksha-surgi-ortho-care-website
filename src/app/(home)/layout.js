fa42e3fa326df21946e6838d081b47e7ebb3d61e
appaddbiz <appaddbiz@gmail.com>
2025-09-24 22:26:52 +0530

    plugin:updated

--- src/app/(home)/layout.js
+++ src/app/(home)/layout.js
@@ -78,7 +78,7 @@ export default function RootLayout({ children }) {
 
         <ThemeProvider>
           {children}
-          <Plugin />
+          
         </ThemeProvider>
         {/* </ThemeSwitcherProvider> */}
       </body>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -11,11 +11,10 @@ import Sidebar from "../Sidebar/Sidebar";
 import ScrollTop from "../ScrollTop/ScrollTop";
 import { usePathname } from "next/navigation";
 import { Toaster } from "react-hot-toast";
+import Plugin from "../Plugin/Plugin";
 
 const Layout = ({ children, pageTitle }) => {
   const [mounted, setMounted] = useState(false);
-
-
 
   useEffect(() => {
     setMounted(true);
@@ -66,15 +65,10 @@ const Layout = ({ children, pageTitle }) => {
     };
   }, [mounted]);
 
-
-
-
   useEffect(() => {
     if (!mounted) return;
 
     AOS.init();
-
-
   }, [mounted]);
 
   if (!mounted) {
@@ -85,8 +79,9 @@ const Layout = ({ children, pageTitle }) => {
     <>
       {/* <CustomCursor /> */}
       {/* <Preloader /> */}
-      <div className='page-wrapper'>
+      <div className="page-wrapper">
         {children}
+        <Plugin />
       </div>
       <MobileNav />
       {/* <Search /> */}
